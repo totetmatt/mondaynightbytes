b790549600e05ba0cd16eb711c39ced8bda58553
totetmatt <matthieu.totet@gmail.com>
2025-03-23 11:05:54 +0100

    Sort date for timetable

--- get_timetable.py
+++ get_timetable.py
@@ -3,14 +3,16 @@ from pathlib import Path
 from requests import get
 import json
 def format_json_output(event):
+
     return {
         "title":event["SUMMARY"],
-        "content": "",
-        "sub_title":event['DTSTART'].dt.strftime('%A %Y-%m-%d %Z  ')
+        "content": event['DTSTART'].dt.strftime('%A %B  %d  %Y'),
+        "sub_title":event['DTSTART'].dt.strftime('%Y-%m-%d %Hh%M %Z')
     }
 if __name__ == "__main__":
     ics_path = "https://api.twitch.tv/helix/schedule/icalendar?broadcaster_id=253129276"
     calendar = icalendar.Calendar.from_ical(get(ics_path).text)
-    timetable = [format_json_output(event) for event in calendar.events]
+    events = sorted(calendar.events,key=lambda a:a['DTSTART'].dt)
+    timetable = [format_json_output(event) for event in events]
 
     json.dump(timetable,open("docs/timetable.json","w"))
